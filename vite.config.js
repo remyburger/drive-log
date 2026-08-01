89a7816f6cb507851f0491095f5b88752f7ebd11
remyburger <remy.burger@gmail.com>
2026-08-01 11:04:19 -0600

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// 👇 Change this to match your GitHub repo name, e.g. "/amelies-drive-log/"
+// If your repo is named "username.github.io" (a user/org site), use "/" instead.
+const REPO_NAME = "/dmv-drive-log/";
+
+export default defineConfig({
+  plugins: [react()],
+  base: REPO_NAME,
+});
